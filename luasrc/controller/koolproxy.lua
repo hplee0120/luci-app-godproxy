95f36ea67859b982d47d34e870155fa5a19ef210
hplee0120 <55278556+hplee0120@users.noreply.github.com>
2021-03-26 18:13:17 +0800

    Update koolproxy.lua

--- luasrc/controller/koolproxy.lua
+++ luasrc/controller/koolproxy.lua
@@ -3,7 +3,7 @@ function index()
 	if not nixio.fs.access("/etc/config/koolproxy")then
 		return
 	end
-	entry({"admin","services","koolproxy"},cbi("koolproxy/global"),_("GodProxy滤广告"),1).dependent=true
+	entry({"admin","services","koolproxy"},cbi("koolproxy/global"),_("广告过滤大师"),1).dependent=true
 	entry({"admin","services","koolproxy","rss_rule"},cbi("koolproxy/rss_rule"), nil).leaf=true
 	entry({"admin","services","koolproxy","status"},call("act_status")).leaf=true
 end
